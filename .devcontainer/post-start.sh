82f917696b93424d7b278b6133e144f984131032
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2025-04-03 17:04:53 +0000

    expose TOdoApp properly

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,6 +4,9 @@ source /workspaces/enablement-live-debugger-bug-hunting/.devcontainer/util/funct
 
 exposeMkdocs
 
+# Wait for todo to be available
+waitForAllPods todoapp
+
 exposeTodoApp
 
 printInfoSection "Your dev.container finished creating"
